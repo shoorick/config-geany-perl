16c847dd8c7b60a88386af06d17c670698393245
Alexander Sapozhnikov <shoorick@mail.ru>
2017-02-07 20:37:19 +0500

    Templates for script and module was added

---
+++ templates/file.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+=head1 NAME
+
+Something
+
+=head1 DESCRIPTION
+
+Some words.
+
+=head1 USAGE
+
+    ./script.pl [ OPTIONS ]
+
+=head1 OPTIONS
+
+=over 4
+
+=item B<-a>, B<--argument>=C<value>
+
+Specify something.
+
+=back
+
+=head1 SEE ALSO
+
+L<http://example.com>
+
+=head1 AUTHOR
+
+{developer} L<{mail}>
+
+=head1 COPYRIGHT
+
+{company}
+
+{year}
+
+=cut
+
+# CREATED {date}
---
+++ templates/module.pm
@@ -0,0 +1,91 @@
+package Pkg::Name;
+
+use strict;
+use warnings;
+use vars qw($VERSION);
+
+$VERSION = '0.01';
+
+=head1 NAME
+
+Something - short summary
+
+=head1 SYNOPSIS
+
+    use Pkg::Name;
+
+    some_function 'argument';
+
+=head1 DESCRIPTION
+
+Some words.
+
+=head1 METHODS
+
+=head2 first
+
+Make something
+
+=head1 SEE ALSO
+
+L<http://example.com>
+
+=head1 AUTHOR
+
+{developer} L<{mail}>
+
+=head1 COPYRIGHT
+
+Copyright {year} {company}
+
+=head1 CONTRIBUTING
+
+This module is developed on Github at:
+
+L<http://github.com/username/pkg-name>
+
+=head1 BUGS
+
+Please report any bugs or feature requests
+to C<bug-pkg-name at rt.cpan.org>, or through the web interface
+at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Pkg-Name>
+or to Github at L<https://github.com/username/pkg-name/issues>
+
+I will be notified, and then
+you'll automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Pkg::Name
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Pkg-Name>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Pkg-Name>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Pkg-Name>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Pkg-Name/>
+
+=item * Public repository at github
+
+L<https://github.com/username/pkg-name>
+
+=back
+
+=cut
+
+# CREATED {date}
